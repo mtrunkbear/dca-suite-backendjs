da81f60b95fb958bf0ddc5341dfe6f4e95ec6fc0
mtrunkbear <migueltroncosogaldames@gmail.com>
2022-05-13 16:29:30 -0400

    if statement improvement

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -1,34 +1,22 @@
 import yahooFinance from 'yahoo-finance2';
 //const query = 'BTC-USD';
-const queryOptions = { period1: '2010-02-01', period2:'2021-02-01',interval:'1mo' };
 
 
 
 export const getDataBySymbol = async (req, res) => {
   const { symbol } = req.params;
-  const body = req.query;
-  console.log(body)
-    const data = await yahooFinance.historical(symbol,queryOptions)
+  const query = req.query;
+  console.log(query)
+if (!query.period1 || !query.period2 || !query.interval ) {
 
-    res.status(200).json(data);
+  console.log('error');
+}
+else{
+   const data = await yahooFinance.historical(symbol,query);
+  return res.status(200).json(data);
+
+  
+}
+    
   };
 
-/*
-
- const datos = getData().then(results => results.map((item)=>item.open));
-
-
-
-
-app.get('/api/historical',(req,res)=>{
-    //getData().then(results => results.map((item)=>item.open)).then(results =>res.json(results))
-    getData().then(results =>res.json(results))
-
-
-})
-const getData =async()=>{
-
-    const data = await yahooFinance.historical(query,queryOptions)
-    return data;
-    
-    } */
