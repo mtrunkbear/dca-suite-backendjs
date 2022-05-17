603bb6f20cfb1180953ee9de4cce70e636ea328d
mtrunkbear <migueltroncosogaldames@gmail.com>
2022-05-16 22:22:42 -0400

    Installed prettier, fnctional improvements

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -1,22 +1,17 @@
-import yahooFinance from 'yahoo-finance2';
-//const query = 'BTC-USD';
-
-
+import yahooFinance from "yahoo-finance2";
 
 export const getDataBySymbol = async (req, res) => {
   const { symbol } = req.params;
   const query = req.query;
-  console.log(query)
-if (!query.period1 || !query.period2 || !query.interval ) {
-
-  console.log('error');
-}
-else{
-   const data = await yahooFinance.historical(symbol,query);
-  return res.status(200).json(data);
-
-  
-}
-    
-  };
-
+  console.log(query, { symbol: symbol });
+  if (!query.period1 || !query.period2 || !query.interval) {
+    console.log("error");
+  } else {
+    try {
+      const data = await yahooFinance.historical(symbol, query);
+      return res.status(200).json(data);
+    } catch (error) {
+      console.error("The query have an Error" + Object.values(error));
+    }
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
-import express from 'express';
+import express from "express";
 
-import cors from 'cors';
+import cors from "cors";
 import dataRoute from "./routes/data.route";
 
 const app = express();
 
 app.listen(4000);
 
-
-
 const corsOptions = {
-    origin: "http://localhost:3000",
+  origin: "http://localhost:3000",
 };
 
 app.use(cors(corsOptions));
 
-
-console.log('Server on PORT:',4000)
-
-
-
+console.log("Server on PORT:", 4000);
 
 app.use("/api/data", dataRoute);
