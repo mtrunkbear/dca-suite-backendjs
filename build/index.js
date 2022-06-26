1914e86e4d36988484ce1eb9117e32c4560df169
mtrunkbear <migueltroncosogaldames@gmail.com>
2022-06-26 00:10:45 -0400

    fixed babel runtime

--- build/index.js
+++ build/index.js
@@ -1,12 +1,12 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 var _express = _interopRequireDefault(require("express"));
 
 var _cors = _interopRequireDefault(require("cors"));
 
 var _data = _interopRequireDefault(require("./routes/data.route"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
 var PORT = process.env.PORT || 4000;
