f1a246357e0c89a83cce6bc71638a64ef83b2f23
Miguel Troncoso <96815554+mtrunkbear@users.noreply.github.com>
2023-06-30 11:58:30 -0400

    Update data.controller.js res handle

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -3,15 +3,15 @@ import yahooFinance from "yahoo-finance2";
 export const getDataBySymbol = async (req, res) => {
   const { symbol } = req.params;
   const query = req.query;
-  console.log(query, { symbol: symbol });
   if (!query.period1 || !query.period2 || !query.interval || !symbol) {
-    console.log("error 400");
+   throw new Error("Bad Request");
   } else {
     try {
       const data = await yahooFinance.historical(symbol, query);
       return res.status(200).json(data);
     } catch (error) {
       console.error("The query have an Error" + Object.values(error));
+       res.status(400).end();
     }
   }
 };
